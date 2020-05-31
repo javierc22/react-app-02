47ecb478599f23e53f4e3ebdc0a07d865bc887dc
javierc22 <javier.ec.22@gmail.com>
2020-05-31 19:05:37 -0400

    Documentando la App

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
-import Error from './Error';
+import PropTypes from 'prop-types'
 import shortid from 'shortid';
+import Error from './Error';
 
 const Formulario = ({ guardarGasto, guardarCrearGasto }) => {
 
@@ -67,6 +68,11 @@ const Formulario = ({ guardarGasto, guardarCrearGasto }) => {
       />
     </form>
   );
+}
+
+Formulario.propTypes = {
+  guardarGasto: PropTypes.func.isRequired,
+  guardarCrearGasto: PropTypes.func.isRequired
 }
  
 export default Formulario;
