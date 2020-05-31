cbb254e375d242c9e9f9949f564e1ff312247fb1
javierc22 <javier.ec.22@gmail.com>
2020-05-31 16:37:08 -0400

    Cargando un componente u otro

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   // Definir el state
   const [ presupuesto, guardarPresupuesto ] = useState(0);
   const [ restante, guardarRestante ] = useState(0);
+  const [ mostrarpregunta, actualizarPregunta ] = useState(true);
 
   return (
     <div className="container">
@@ -14,19 +15,25 @@ function App() {
         <h1>Gasto semanal</h1>
 
         <div className="contenido-principal contenido">
-          <Pregunta
-            guardarPresupuesto={ guardarPresupuesto }
-            guardarRestante={ guardarRestante }
-          />
+          { mostrarpregunta ?
+           (
+             <Pregunta
+               guardarPresupuesto={ guardarPresupuesto }
+               guardarRestante={ guardarRestante }
+               actualizarPregunta={actualizarPregunta}
+             />
+           ) :
+           (
+            <div className="row">
+              <div className="one-half column">
+                <Formulario />
+              </div>
+              <div className="one-half column">
 
-          <div className="row">
-            <div className="one-half column">
-              <Formulario />
+              </div>
             </div>
-            <div className="one-half column">
-
-            </div>
-          </div>
+           )
+          }
         </div>
       </header>
     </div>
--- src/components/Pregunta.jsx
+++ src/components/Pregunta.jsx
@@ -1,7 +1,7 @@
 import React, { Fragment, useState } from 'react';
 import Error from './Error';
 
-const Pregunta = ({ guardarPresupuesto, guardarRestante }) => {
+const Pregunta = ({ guardarPresupuesto, guardarRestante, actualizarPregunta }) => {
 
   // Definir el State
   const [ cantidad, guardarCantidad ] = useState(0);
@@ -24,9 +24,9 @@ const Pregunta = ({ guardarPresupuesto, guardarRestante }) => {
 
     // Si pasa la validación
     guardarError(false);
-
     guardarPresupuesto(cantidad);
     guardarRestante(cantidad);
+    actualizarPregunta(false);
   }
 
   return (
